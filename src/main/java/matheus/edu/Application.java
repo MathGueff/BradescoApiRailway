055595f302886af727d85328ea980d919b90fbda
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-05-18 18:55:52 -0300

    Fix cors

--- src/main/java/matheus/edu/Application.java
+++ src/main/java/matheus/edu/Application.java
@@ -1,8 +1,10 @@
 package matheus.edu;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(servers = {@Server(url="/", description="default server api")})
 @SpringBootApplication
 public class Application {
 
